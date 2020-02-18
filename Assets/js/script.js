a80b94cca254dee2e81ac8add323c9c8f8007fe2
Vincent De Gheyndt <vincent.degheyndt@gmail.com>
2020-02-18 10:13:20 +0100

    modifications

---
+++ Assets/js/script.js
@@ -0,0 +1,48 @@
+var chaine = "Hackeurs Poulette"; 
+var nb_car = chaine.length; 
+var tableau = chaine.split("");
+texte = new Array;
+var txt = '';
+var nb_msg = nb_car - 1;
+for (i=0; i<nb_car; i++) {
+texte[i] = txt+tableau[i];
+var txt = texte[i];
+}
+
+actual_texte = 0;
+function changeMessage()
+{
+document.getElementById("h1").innerHTML = texte[actual_texte];
+actual_texte++;
+if(actual_texte >= texte.length)
+actual_texte = nb_msg;
+}
+if(document.getElementById)
+
+setInterval("changeMessage()",200) /* la vitesse de defilement (plus on a une valeur faible plus 
+texte s'affiche rapidement) */
+
+var form = document.querySelector("form");
+var log = document.querySelector("#log");
+
+form.addEventListener("submit", function(event) {
+  var data = new FormData(form);
+  var output = "";
+  for (const entry of data) {
+    output = entry[0] + "=" + entry[1] + "\r";
+  };
+  log.innerText = output;
+  event.preventDefault();
+}, false);
+
+function controlvide() {
+  for (i=0; i<document.getElementsByTagName("sexe").length; i++)
+  if (document.getElementsByTagName("sexe")[i].name=="test_radio")
+  if (document.getElementsByTagName("sexe")[i].checked) {
+  return true;
+  }
+  else
+  {
+  alert("Vous devez cocher une case !");
+  return false;
+  }}
---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="Assets/css/style.css">
+    <link rel="stylesheet" href="Assets/php/formulaire.php">
+    <title>Hackers Poulette</title>
+</head>
+<body>
+<header>
+<nav>
+<a href="#produit"><b>Produits</b></a>
+<a href="#acceuille"><b>Acceuille</b></a>
+<a href="#contacte"><b>Contacte</b></a>
+</nav>
+</header>
+<!--?*Ici premiere partie de la page-->
+<div id="fond">
+    <div id="acceuille" class="acceuille container-fluid">
+        <div class="row">
+            <div class="titre col-sm-12 col-md-5 text-center order-2">
+                <img class="logo" class="img"src="Assets/img/hackers-poulette-logo.png" alt="logo,text,image">
+                <h3 class="text-light">We Raspberry</h3>
+            </div>
+            
+            <div class="who col-sm-12 col-md-3 text-center order-1 shadow-lg">
+            <div class="box1">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal"><b>A propos</b></h4>
+                </div>
+                <div class="card-body1">
+                    <p><b>Hackers Poulette</b> est un site conçu pour les debutants, les fans et les veterans des rasberry ! Nous metons tout en oeuvre pour que nos client qui soit-il puisse obtenir ce nano-ordinateur et l'utiliser, le comprendre sur toute ses coutures. Nous offrons un commerce mondial afin que tout le monde ou qu'il soit puisse en obtenir un ! Fraichement née en 2017 <b>Hackers Poulette</b> ce lance dans la conquete du monde avec les Rasperry Pi avec ce nouveau site web.</p>
+                    <a href="#produit"> Alors n'hésiter plus et venez voir nos produit !</a>
+                </div>
+            </div>
+            </div>
+           
+            <div class="produit col-sm-12 col-md-3 text-center shadow-lg order-3">
+            <div class="box1">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal"><b>Produit</b></h4>
+                </div>
+                <div class="card-body1">
+                    <p> Le Raspberry Pi est un nano-ordinateur monocarte à processeur ARM conçu par des professeurs du département informatique de l'université de Cambridge dans le cadre de la fondation Raspberry Pi2. Cet ordinateur, de la taille d'une carte de crédit, est destiné à encourager l'apprentissage de la programmation informatique2. Il permet l'exécution de plusieurs variantes du système d'exploitation libre GNU/Linux, notamment Debian, et des logiciels compatibles. Mais il fonctionne également avec Windows 10 IoT Core3 et celui de Google, Android Pi4.</p>
+                </div>
+            </div>
+            </div>
+            <div class="info col-sm-12 col-md-12 text-center shadow-lg order-4">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal"><b>Mais aussi !</b></h4>
+                </div>
+                <div class="card-body1">
+                    <p>Afin de permettre a tous d'utiliser un Rasperry Pi nous avons fait appel a different associiation qui vous permettrons d'apprendre a utiliser un Rasperry Pi ! Et oui beaucoup de gens sont intérresser par ce nano-ordinateur, pourquoi ? car il permet différente chose, tres interressant les une des autres ! Mais nous n'allons pas vous retirer ce mystere héhé. Nous vous laissons pousser votre creativité sur ce petit objet. </p>
+                    <ul class="list-unstyled">
+                        <li><a href="https://codeclub.org/en/">Code Club</a></li>
+                        <li><a href="https://coderdojo.com/fr-FR">Coder Dojo</a></li>
+                        <li><a href="https://www.raspberrypi.org/jam/">Rasperry Jam</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <!--?*ici ce trouve la deuxieme partie de la page-->
+    <div class="fond2">
+<div class="container">
+    <h1 id="produit" class="col-md-12 text-center text-light"><b>Produits</b></h1>
+        <div class="card-deck mb-3 text-center">
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Raspberry Pi Bs</h4>
+                </div>
+                <div class="card-body">
+                    <img id="img" src="Assets/img/Raspberry_Pi_Model_B.jpg" alt="Raspberry Pi B">
+                    <ul class="list-unstyled mt-3 mb-4">
+                        <li>2 GB of storage</li>
+                        <li>empty</li>
+                        <li>empty</li>
+                    </ul>
+                    <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
+                </div>
+            </div>
+        
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Raspberry PI B+</h4>
+            </div>
+            <div class="card-body">
+            <img id="img" src="Assets/img/Raspberry_Pi_B+.jpg" alt="Raspberry PI B+">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>10 GB of storage</li>
+                    <li>empty</li>
+                    <li>empty</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
+            </div>
+        </div>
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Raspberry Pi 2</h4>
+            </div>
+        <div class="card-body">
+        <img id="img" src="Assets/img/Raspberry_Pi_2.jpg" alt="Raspberry Pi 2">
+            <ul class="list-unstyled mt-3 mb-4">
+                <li>15 GB of storage</li>
+                <li>empty</li>
+                <li>empty</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="card-deck mb-3 text-center">
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Raspberry Pi Bs</h4>
+                </div>
+                <div class="card-body">
+                <img id="img" src="Assets/img/Raspberry_Pi_3.JPG" alt="Raspberry Pi 3">
+                    <ul class="list-unstyled mt-3 mb-4">
+                        <li>2 GB of storage</li>
+                        <li>empty</li>
+                        <li>empty</li>
+                    </ul>
+                    <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
+                </div>
+            </div>
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Raspberry Pi 3B+</h4>
+                </div>
+                <div class="card-body">
+                <img id="img" src="Assets/img/Raspberry_Pi_3_b+.jpg" alt="Raspberry Pi 3B+">
+                    <ul class="list-unstyled mt-3 mb-4">
+                        <li>10 GB of storage</li>
+                        <li>empty</li>
+                        <li>empty</li>
+                    </ul>
+                    <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
+                </div>
+            </div>
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Raspberry Pi 2</h4>
+                </div>
+            <div class="card-body">
+            <img id="img" src="Assets/img/Raspberry_Pi_4.jpg" alt="Raspberry Pi 4">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>15 GB of storage</li>
+                    <li>empty</li>
+                    <li>empty</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+    <!-- Ici ce trouve le formulaire-->
+    <div class="fond3">
+    <div class="container-fluid">
+        <div class="row">
+            <form id="contacte"method="post" action="index.php" class="formulaire col-md-12 col-sm-12 text-center align-content-center">
+                
+                <div id="tv" >
+                    <div class="col-sm-12 col-sm-offset-4" id="form">
+                        <div class="well" style="margin-top: 10%;">
+                        
+                        <h1 class="titre" class="com-md-12 text-center text-light "><b>Formulaire de contacte :</b></h1>
+                        <form role="form" id="contactForm" data-toggle="validator" class="shake" method="post" action="index.php">
+
+                        <label for="sujet du message"><b>Sujet du message</b></label><br />
+                        <div class="form-group col-sm-6"></div>
+                        <select name="sujet" id="sujet message">
+                                <option value="J'ai une question concernant vos produits">J'ai une question concernant vos produits</option>
+                                <option value="J'ai une question concernant votre site">J'ai une question concernant votre site</option>
+                                <option value="J'ai une question pour vous ennuyer">J'ai une question pour vous ennuyer</option>
+                        </select>
+                                <div class="checkbox">
+                                        <div class="form-group col-sm-6"></div>
+                                            
+                                            <label for="sexe">Sexe : </label>
+                                            <input type="radio"required name="sexe" value="Homme" id="homme"> Homme <input required type="radio" name="sexe" value="Femme" id="femme"> Femme
+                                        </div>
+                                </div>
+                            <div class="row">
+                                
+                                <div class="form-group col-sm-6">
+                                    <label for="nom" class="h4" >Nom</label>
+                                    <input type="text" class="form-control" id="nom" name="nom" placeholder="Entrez votre nom" required data-error="entrez votre nom">
+                                </div>
+                                <div class="form-group col-sm-6">
+                                    <label for="prenom" class="h4" >Prénom</label>
+                                    <input type="text" name="prenom" class="form-control" id="prenom" placeholder="Entrez votre prénom" required data-error="NEW ERROR MESSAGE">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                                <div class="form-group col-sm-6">
+                                    <label for="email" class="h4" >Email</label>
+                                    <input type="email" name="email" class="form-control" id="email" placeholder="Entrez votre adresse email" required>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                                <div class="form-group col-sm-6">
+                                        <label for="pays" class="h4" >Pays</label>
+                                        <input name="pays" type="text" class="form-control" id="pays" placeholder="pays" required>
+                                        <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="message" class="h4" >Message</label>
+                                <textarea name="message" id="message" class="form-control" rows="5" placeholder="Entrez votre message" required></textarea>
+                                <div class="help-block with-errors"></div>
+                            </div>
+                            <button type="submit" id="form-submit" class="btn btn-success btn-lg pull-right " name="valide">Submit</button>
+                            <div id="msgSubmit" class="h3 text-center hidden"></div>
+                            <div class="clearfix"></div>
+                            <div class="form-group" id="honey">
+                                <label> non </label>
+                                <intput type="text" name="honey" class="form"/>
+                            </div>
+                        </div>
+                        </form>
+                        
+                    </div>
+
+                </div>
+
+                
+
+            </div>
+
+            
+        </div>
+            <footer>
+                   <p>Posted by: De Paola Quentin & Vissers Vincent</p>
+                   <p>Contact information: <a href="mailto:quentindepaola404@gmail.com">quentindepaola404@gmail.com</a>.</p>
+            </footer>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="Assets/js/script.js"></script>
+    
+</body>
+</html>
+
+<?php
+if(isset($_POST['submit'])){
+   $errorMSG ="";
+
+       // question
+
+         $sujet = filter_var($_POST['sujet'], FILTER_SANITIZE_STRING);
+
+       //sexe
+
+
+       $sexe = filter_var($_POST['sexe'], FILTER_SANITIZE_STRING);
+
+        // nom
+         
+     
+          $nom = filter_var($_POST['nom'], FILTER_SANITIZE_STRING);
+          
+          
+     
+        // prenom
+         
+        
+          $prenom = filter_var($_POST['prenom'], FILTER_SANITIZE_STRING);
+     
+         
+     
+        // EMAIL
+         
+     
+          $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        
+        //PAYS
+    
+        $pays = filter_var($_POST['pays'], FILTER_SANITIZE_STRING);
+         
+     
+        // MESSAGE
+         
+         
+             $messages =filter_var($_POST['messages'], FILTER_SANITIZE_STRING);
+     
+     $EmailTo = "quentindepaola404@gmail.com";
+     
+     // preparation de l email 
+        $Body = "";
+        $Body .= "\n";
+        $Body .= "sexe: ";
+        $Body .= $sexe;
+        $Body .= "\n";
+        $Body .= "nom: ";
+        $Body .= $nom;
+        $Body .= "\n";
+        $Body .= "prenom: ";
+        $Body .= $prenom;
+        $Body .= "pays: ";
+        $Body .= $pays;
+        $Body .= "\n";
+        $Body .= "messages: ";
+        $Body .= $messages;
+        $Body .= "\n";
+         
+         
+         
+         
+           
+        // send email
+        $success = mail($EmailTo, $sujet, $Body, "From:".$email);
+        // redirect to success page
+        if ($success == ""){
+            echo '<script>alert("Formulaire envoyé");</script>';
+        }else{
+            if($errorMSG == ""){
+                echo "Something went wrong :(";
+            } else {
+                echo $errorMSG;
+            }
+        }
+    }
+
+?>
